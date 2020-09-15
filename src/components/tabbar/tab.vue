66eb9d2750af43a3d664688d19eadd30031ed8f9
liming1994 <2385121351@qq.com>
2020-09-15 12:36:48 +0800

    caidan1

--- src/components/tabbar/tab.vue
+++ src/components/tabbar/tab.vue
@@ -1,6 +1,5 @@
 <template>
-
-        <div style="width: 207px;height: 100%;">
+        <div class="mebubar">
             <a-menu
                     :default-selected-keys="['1']"
                     :default-open-keys="['sub1']"
@@ -9,29 +8,59 @@
                     :inline-collapsed="$store.state.collapsed">
                 <a-menu-item key="1">
                     <a-icon type="mail" />
-                   <span>Navigation One</span>
+                   <span>首页</span>
                 </a-menu-item>
                 <a-menu-item key="2">
                     <a-icon type="calendar" />
-                    <span>Navigation Two</span>
+                    <span>租户管理</span>
+                </a-menu-item>
+                <a-menu-item key="3">
+                    <a-icon type="calendar" />
+                    <span>访客管理</span>
+                </a-menu-item>
+                <a-menu-item key="4">
+                    <a-icon type="calendar" />
+                    <span>放行条管理</span>
+                </a-menu-item>
+                <a-menu-item key="5">
+                    <a-icon type="calendar" />
+                    <span>设备维修</span>
+                </a-menu-item>
+                <a-menu-item key="6">
+                    <a-icon type="calendar" />
+                    <span>缴费管理</span>
+                </a-menu-item>
+                <a-menu-item key="7">
+                    <a-icon type="calendar" />
+                    <span>缴费查询</span>
                 </a-menu-item>
                 <a-sub-menu key="sub1">
-                    <span slot="title"><a-icon type="appstore" /><span>Navigation Three</span></span>
-                    <a-menu-item key="3">
-                        Option 3
+                    <span slot="title"><a-icon type="appstore" />
+                        <span>设备维保</span>
+                    </span>
+                    <a-menu-item key="8">
+                        巡检参数
                     </a-menu-item>
-                    <a-menu-item key="4">
-                        Option 4
+                    <a-menu-item key="9">
+                        保养参数
                     </a-menu-item>
-                    <a-sub-menu key="sub1-2" title="Submenu">
-                        <a-menu-item key="5">
-                            Option 5
-                        </a-menu-item>
-                        <a-menu-item key="6">
-                            Option 6
-                        </a-menu-item>
-                    </a-sub-menu>
+                    <a-menu-item key="10">
+                        巡保模块
+                    </a-menu-item>
+                    <a-menu-item key="11">
+                        设备台账
+                    </a-menu-item>
+                    <a-menu-item key="12">
+                        巡保任务
+                    </a-menu-item>
+                    <a-menu-item key="13">
+                        图片管理
+                    </a-menu-item>
                 </a-sub-menu>
+                <a-menu-item key="14">
+                    <a-icon type="calendar" />
+                    <span>缴费查询</span>
+                </a-menu-item>
                 <a-sub-menu key="sub2">
                     <span slot="title"><a-icon type="setting" /><span>Navigation Four</span></span>
                     <a-menu-item key="7">
@@ -56,7 +85,7 @@
         name: "tab",
         data(){
             return {
-                mode: 'vertical',
+                mode: 'inline',
                 theme: 'dark',
 
             }
@@ -68,5 +97,9 @@
 </script>
 
 <style scoped lang="scss">
+    .mebubar{
+        height: 100%;
+        background-color: rgb(0,12,23);
 
+    }
 </style>
